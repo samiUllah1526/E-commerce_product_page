c1e57868f6a3446117b5ec062471c6fb7276f965
Sami Ullah <sami.naazar1526@gmail.com>
2021-11-12 17:51:57 +0500

    Make Images slideable

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import iconMenu from "./images/icon-menu.svg";
 // import iconCart from "./images/icon-cart.svg";
+import { useState, useEffect } from "react";
 import iconPlus from "./images/icon-plus.svg";
 import iconMinus from "./images/icon-minus.svg";
 import iconCart from "./images/icon-cart.svg";
@@ -8,8 +9,21 @@ import { MobileSlider } from "./components/MobileSlider";
 import { Description } from "./components/Description";
 import { PriceBar } from "./components/PriceBar";
 function App() {
+  // const [test, setTest] = useState("");
+  // const [error, setError] = useState("i am error");
+  // console.log("error", error);
+  // useEffect(() => {
+  //   setError(!error);
+  // }, [test]);
+
   return (
     <>
+      {/* <input
+        type="text"
+        placeholder="d"
+        value={test}
+        onChange={(e) => setTest(e.target.value)}
+      /> */}
       <Navbar />
       <MobileSlider />
       <Description />
@@ -17,20 +31,22 @@ function App() {
       <div className="container w-11/12 ">
         <div className="flex items-center justify-between rounded-lg h-14 px-5 bg-mentor-lightGrayishBlue">
           <a className="block">
-            <img src={iconPlus} alt="" />{" "}
+            <img src={iconMinus} alt="iconMinus" />{" "}
           </a>
-          <div>0</div>
+          <div className="font-bold">0</div>
           <a className="block">
-            <img src={iconMinus} alt="" />{" "}
+            <img src={iconPlus} alt="iconPlus" />{" "}
           </a>
         </div>
-        <div className="flex justify-center items-center rounded-lg h-14 px-5 bg-mentor-orange text-white">
+        <div className="my-4 flex justify-center items-center rounded-lg h-14 px-5 bg-mentor-orange text-white">
           <div className="mr-3">
-            <img className="w-5 h-5" src={iconCart} alt="cart_icon" />
+            <img className="w-5 h-5" src={iconCart} alt="iconCart" />
           </div>
-          <button className="font-bold tex-base" type="submit">
-            Add to cart
-          </button>
+          <div>
+            <button className="font-bold text-base" type="submit">
+              Add to cart
+            </button>
+          </div>
         </div>
       </div>
     </>
--- src/components/MobileSlider.jsx
+++ src/components/MobileSlider.jsx
@@ -1,33 +1,70 @@
-import React from "react";
-import product1 from "./../images/image-product-1.jpg";
+import React, { useState } from "react";
 import next from "./../images/icon-next.svg";
-import previous from "./../images/icon-previous.svg";
 
 export function MobileSlider() {
+  let [imageIndex, setImageIndex] = useState(1);
+  // const productLinks = [
+  //   "image-product-1.jpg",
+  //   "image-product-2.jpg",
+  //   "image-product-3.jpg",
+  //   "image-product-4.jpg",
+  // ];
+
+  const previousImage = () => {
+    if (imageIndex == 1) {
+      setImageIndex(productLinks.length);
+    } else {
+      setImageIndex((imageIndex -= 1));
+    }
+  };
+
+  const nextImage = () => {
+    console.log("next clicked");
+    if (imageIndex != productLinks.length) {
+      setImageIndex((imageIndex += 1));
+    } else {
+      setImageIndex(1);
+    }
+  };
   return (
     <>
       <div className="relative">
         <div>
           <img
-            src={product1}
-            alt="product1"
+            src={`${process.env.PUBLIC_URL}/images/image-product-${imageIndex}.jpg`}
+            alt="product"
             className="object-cover object-center w-full h-55"
           />
+          {/* <img
+            src={require(`./../images/products/${productLinks[0]}`).default}
+            alt="product"
+            className="object-cover object-center w-full h-55"
+          /> */}
         </div>
         <div className="rounded-full h-10 w-10 absolute inset-y-1/2 left-5 bg-white flex justify-center items-center">
           <img
+            onClick={previousImage}
             src={next}
             alt="previous"
             className="transform rotate-180 w-3/10 h-1/3"
           />
+          {console.log("imageIndex==>", imageIndex)}
         </div>
 
         {/* NEXT */}
         <div className="rounded-full h-10 w-10  absolute inset-y-1/2 right-5 bg-white flex justify-center items-center">
-          <img src={next} alt="next" className="w-3/10 h-1/3" />
+          <img
+            onClick={nextImage}
+            src={next}
+            alt="next"
+            className="w-3/10 h-1/3"
+          />
+          {console.log("imageIndex==>", imageIndex)}
         </div>
       </div>
-      <h1 className="container w-11/12 font-sans font-bold text-mentor-orange pt-5 pb-3">SNEAKERS COMPANY</h1>
+      <h1 className="container w-11/12 font-sans font-bold text-mentor-orange pt-5 pb-3">
+        SNEAKERS COMPANY
+      </h1>
     </>
   );
 }
